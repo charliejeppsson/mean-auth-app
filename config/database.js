7bcae38730d891e2afcbcf6cb1dc2b0bb41329fa
Charlie Jeppsson <charlie.jeppsson1@gmail.com>
2017-08-17 01:18:29 +0200

    add mLab database cofig

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-  database: 'mongodb://localhost:27017/meanauth',
+  database: 'mongodb://charlie.jeppsson1@gmail.com:Hundkoja1337@ds145283.mlab.com:45283/meanauthapp',
   secret: 'yoursecret'
 }
