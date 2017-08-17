ce6a356f552e89d61f1f2a675789d6b54a47d0fd
Charlie Jeppsson <charlie.jeppsson1@gmail.com>
2017-08-17 17:10:10 +0200

    add search bar to home page, change name

--- angular-src/src/app/app.module.ts
+++ angular-src/src/app/app.module.ts
@@ -1,9 +1,12 @@
+// Modules
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { RouterModule, Routes } from '@angular/router';
+import { FlashMessagesModule } from 'angular2-flash-messages';
 
+// Components
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { LoginComponent } from './components/login/login.component';
@@ -11,10 +14,11 @@ import { RegisterComponent } from './components/register/register.component';
 import { HomeComponent } from './components/home/home.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { ProfileComponent } from './components/profile/profile.component';
+import { SearchComponent } from './components/search/search.component';
 
+// Services
 import { ValidateService } from './services/validate.service';
 import { AuthService } from './services/auth.service';
-import { FlashMessagesModule } from 'angular2-flash-messages';
 import { AuthGuard } from './guards/auth.guard';
 
 const appRoutes: Routes = [
@@ -33,7 +37,8 @@ const appRoutes: Routes = [
     RegisterComponent,
     HomeComponent,
     DashboardComponent,
-    ProfileComponent
+    ProfileComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
--- config/database.js
+++ config/database.js
@@ -1,4 +1,5 @@
 module.exports = {
-  database: 'mongodb://charliejeppsson:12345678@ds145283.mlab.com:45283/meanauthapp',
+  // database: 'mongodb://charliejeppsson:12345678@ds145283.mlab.com:45283/meanauthapp',
+  database: 'mongodb://localhost:27017/meanauth',
   secret: 'yoursecret'
 }
