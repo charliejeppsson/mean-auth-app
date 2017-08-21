938b3a360e3ec15d02b72f819a473675a52f82e9
Charlie Jeppsson <charlie.jeppsson1@gmail.com>
2017-08-21 15:05:24 +0200

    cors config

--- app.js
+++ app.js
@@ -35,10 +35,8 @@ const port = process.env.PORT || 8080;
 // app.use(cors(corsOptions));
 app.use(cors());
 
-app.use(function(req, res, next) {
+app.use(function(req, res) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
 });
 
 // Set static folder
