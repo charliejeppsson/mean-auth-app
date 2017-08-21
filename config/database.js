e268bde4de718dda353c481c33bfccf2f200058b
Charlie Jeppsson <charlie.jeppsson1@gmail.com>
2017-08-21 22:04:08 +0200

    add banner photo for home page

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
-  database: 'mongodb://charliejeppsson:12345678@ds145283.mlab.com:45283/meanauthapp',
-  // database: 'mongodb://localhost:27017/meanauth',
+  // database: 'mongodb://charliejeppsson:12345678@ds145283.mlab.com:45283/meanauthapp',
+  database: 'mongodb://localhost:27017/meanauth',
   secret: 'yoursecret'
 }
